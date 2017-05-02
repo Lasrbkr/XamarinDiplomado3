b55e2e8d517ef245cea6be1459c55d6231da6fe3
LuisSalas <luis.salas@permiafiglia.com>
2017-05-02 11:12:34 -0500

    * AndroidAppLab04.csproj: Lab04 PCL : )
    * Lab1-Start.sln:
    * IDialog.cs:
    * AppValidator.cs:
    * PCLProject.csproj:
    * packages.config:
    * MainActivity.cs:
    * AndroidDialog.cs:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * AssemblyInfo.cs:
    * Main.axml:
    * Strings.xml:
    * AboutResources.txt:
    * AndroidManifest.xml:
    * Resource.designer.cs:

---
+++ AndroidAppLab04/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidAppLab04
+{
+    [Activity(Label = "AndroidAppLab04", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.Main);
+
+            var Validator = new PCLProject.AppValidator(new AndroidDialog(this));
+            Validator.EMail = "tuusuario";
+            Validator.Password = "tucontraseña";
+            Validator.Device = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+            Validator.Validate();
+        }
+    }
+}
---
+++ PCLProject/AppValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace PCLProject
+{
+    public class AppValidator
+    {
+        IDialog Dialog;
+        public string EMail { get; set; }
+        public string Password { get; set; }
+        public string Device { get; set; }
+
+        public AppValidator(IDialog platformDialog)
+        {
+            Dialog = platformDialog;
+        }
+
+        public async void Validate()
+        {
+            string Result;
+            var ServiceClient = new SALLab04.ServiceClient();
+            var SvcResult = await ServiceClient.ValidateAsync(EMail, Password, Device);
+            /* Aquí se puede implementar la funcionalidad
+             principal de la clase. Por el momento solo devuelve
+             una cadena fija. */
+            Result = $"{SvcResult.Status}\n{SvcResult.Fullname}\n{SvcResult.Token}";//"¡Aplicación validada!";
+            /* Invocar el código específico de la plataforma */
+            Dialog.Show(Result);
+        }
+    }
+}
