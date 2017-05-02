4c24da765d831568fe178216dff633a0a8ee5cd4
LuisSalas <luis.salas@permiafiglia.com>
2017-05-02 10:40:29 -0500

    * AndroidAppLab03.csproj: Lab03 Shared Project
    * Lab1-Start.sln:
    * MainActivity.cs:
    * packages.config:
    * MySharedCode.cs:
    * AboutAssets.txt:
    * Main.axml:
    * AssemblyInfo.cs:
    * Strings.xml:
    * AboutResources.txt:
    * SharedProjectLab03.shproj:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * SharedProjectLab03.projitems:

---
+++ AndroidAppLab03/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidAppLab03
+{
+    [Activity(Label = "AndroidAppLab03", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.Main);
+
+            var Helper = new SharedProjectLab03.MySharedCode();
+            new AlertDialog.Builder(this).SetMessage(Helper.GetFilePath("demo.dat")).Show();
+
+            //Validate();
+        }
+
+        private async void Validate()
+        {
+            SALLab03.ServiceClient ServiceClient = new SALLab03.ServiceClient();
+            string StudentEmail = "tucorreo";
+            string Password = "tucontraseña";
+            string myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+
+            SALLab03.ResultInfo Result = await ServiceClient.ValidateAsync(StudentEmail, Password, myDevice);
+
+            Android.App.AlertDialog.Builder Builder = new AlertDialog.Builder(this);
+            AlertDialog Alert = Builder.Create();
+            Alert.SetTitle("Resultado de la verificación");
+            Alert.SetMessage($"{Result.Status}\n{Result.Fullname}\n{Result.Token}");
+            Alert.SetButton("Ok", (s, ev) => { });
+            Alert.Show();
+        }
+    }
+}
---
+++ SharedProjectLab03/MySharedCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace SharedProjectLab03
+{
+    public class MySharedCode
+    {
+        public string GetFilePath(string fileName)
+        {
+            string FilePath = "";
+#if WINDOWS_UWP
+            FilePath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path,filename);
+#else
+#if __ANDROID__
+            string LibraryPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            FilePath = Path.Combine(LibraryPath, fileName);
+#endif
+#endif
+
+            return FilePath;
+        }
+    }
+}
